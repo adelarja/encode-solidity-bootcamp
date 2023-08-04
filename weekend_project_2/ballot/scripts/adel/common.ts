6c64f426cf18da4e451aeefafbec4bff7f93319d
lucasespinosa28 <52639395+lucasespinosa28@users.noreply.github.com>
2023-08-04 12:56:08 -0300

    add support to wallet with private key or mnemonic

--- weekend_project_2/ballot/scripts/adel/common.ts
+++ weekend_project_2/ballot/scripts/adel/common.ts
@@ -3,36 +3,53 @@ import { Ballot, Ballot__factory } from "../../typechain-types";
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-export async function getWallet() {
-    const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL);
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
-  
-    console.log(`Using address ${wallet.address}`);
-  
-    const balanceBN = await provider.getBalance(wallet.address);
-    const balance = Number(ethers.formatUnits(balanceBN));
-  
-    console.log(`Wallet balance ${balance}`);
-  
-    if (balance < 0.01) {
-  
-      throw new Error("Not enough ether");
-  
+const loadWallet = (provider: ethers.JsonRpcProvider) => {
+  if (process.env.PRIVATE_KEY != undefined) {
+    return new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  }
+
+  if (process.env.MNEMONIC != undefined) {
+    if (typeof process.env.MNEMONIC === 'string' && process.env.MNEMONIC.length > 0) {
+      const MNEMONIC: string = process.env.MNEMONIC;
+      return ethers.Wallet.fromPhrase(MNEMONIC, provider);
     }
-  
-    return wallet;
+
+  }
+
+  return ethers.Wallet.createRandom(provider);
 }
-  
+export async function getProvider(){
+  return new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL);
+}
+export async function getWallet() {
+  const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL);
+  const wallet = loadWallet(provider);
+  console.log(`Using address ${wallet.address}`);
+
+  const balanceBN = await provider.getBalance(wallet.address);
+  const balance = Number(ethers.formatUnits(balanceBN));
+
+  console.log(`Wallet balance ${balance}`);
+
+  if (balance < 0.01) {
+
+    throw new Error("Not enough ether");
+
+  }
+
+  return wallet;
+}
+
 export function getProposals() {
-    const proposals = process.argv.slice(2);
-  
-    if (proposals.length < 2) {
-  
-      throw new Error("Need at least 2 proposals.");
-  
-    }
-  
-    return proposals;
+  const proposals = process.argv.slice(2);
+
+  if (proposals.length < 2) {
+
+    throw new Error("Need at least 2 proposals.");
+
+  }
+
+  return proposals;
 }
 
 export async function deployContract() {
