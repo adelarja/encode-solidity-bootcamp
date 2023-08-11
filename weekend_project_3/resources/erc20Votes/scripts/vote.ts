1d3d14993a0865139812f52dbfa70e4a22b7f98a
adelarja <adelarja@gmail.com>
2023-08-10 22:30:30 -0300

    Added votes script

---
+++ weekend_project_3/resources/erc20Votes/scripts/vote.ts
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv';
+import { getTokenizedBallotContractAt } from "./common";
+dotenv.config();
+
+async function main() {
+  const [contractAddres, proposal, amount] = process.argv.slice(2);
+
+  console.log(`Voting on contract: ${contractAddres} for proposal ${proposal} `);
+
+  const ballotContract = await getTokenizedBallotContractAt(contractAddres);
+  await ballotContract.vote(proposal, amount);
+  
+  console.log("Voted!");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
