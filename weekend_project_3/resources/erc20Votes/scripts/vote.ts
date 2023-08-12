3ebfbf42e826eb56791e39fcce5b38b95dc208a8
rayman1104 <rayman1104@gmail.com>
2023-08-12 17:48:24 +0200

    add checkVotingPower & viewProposals scripts

--- weekend_project_3/resources/erc20Votes/scripts/vote.ts
+++ weekend_project_3/resources/erc20Votes/scripts/vote.ts
@@ -3,11 +3,11 @@ import { getTokenizedBallotContractAt } from "./common";
 dotenv.config();
 
 async function main() {
-  const [contractAddres, proposal, amount] = process.argv.slice(2);
+  const [contractAddress, proposal, amount] = process.argv.slice(2);
 
-  console.log(`Voting on contract: ${contractAddres} for proposal ${proposal} `);
+  console.log(`Voting on contract: ${contractAddress} for proposal ${proposal} `);
 
-  const ballotContract = await getTokenizedBallotContractAt(contractAddres);
+  const ballotContract = await getTokenizedBallotContractAt(contractAddress);
   await ballotContract.vote(proposal, amount, {gasLimit: 50000});
   
   console.log("Voted!");
