9865cb943c957756325e6ba7086c9f940bd20020
rayman1104 <rayman1104@gmail.com>
2023-08-19 21:02:51 +0200

    fix DTOs

--- weekend_project_4/resources/backend/src/dtos/deployBallot.dto.ts
+++ weekend_project_4/resources/backend/src/dtos/deployBallot.dto.ts
@@ -1,8 +1,11 @@
 import { ArrayMinSize, ArrayUnique, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class DeployBallotDto {
+  @ApiProperty()
   @IsNotEmpty()
   address: string;
+  @ApiProperty()
   @ArrayMinSize(2)
   @ArrayUnique()
   proposals: string[];
