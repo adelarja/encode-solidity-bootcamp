02b22441073d8f658ad165259ad840a826dd3376
lucasespinosa28 <52639395+lucasespinosa28@users.noreply.github.com>
2023-09-05 17:20:14 -0300

    cleaning contract

--- weekend_project_7/NFT/contracts/NFT.sol
+++ weekend_project_7/NFT/contracts/NFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import {UsingTellor} from "usingtellor/contracts/UsingTellor.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721, UsingTellor {
     using Counters for Counters.Counter;
@@ -17,6 +17,32 @@ contract NFT is ERC721, UsingTellor {
     constructor(
         address payable _tellorAddress
     ) ERC721("bear and bull", "BB") UsingTellor(_tellorAddress) {}
+    
+    function getEthSpotPrices() public view returns (uint256, uint256) {
+        bytes memory _queryData = abi.encode(
+            "SpotPrice",
+            abi.encode("eth", "usd")
+        );
+        bytes32 _queryId = keccak256(_queryData);
+        (
+            bytes memory _valuePast,
+            uint256 _timestampPastRetrieved
+        ) = getDataBefore(_queryId, block.timestamp - 24 hours);
+        (
+            bytes memory _valuePresent,
+            uint256 _timestampPresentRetrieved
+        ) = getDataBefore(_queryId, block.timestamp - 20 minutes);
+
+        if (_timestampPresentRetrieved == 0 && _timestampPastRetrieved == 0)
+            return (0, 0);
+        require(block.timestamp - _timestampPastRetrieved < 48 hours);
+        require(block.timestamp - _timestampPresentRetrieved < 24 hours);
+
+        return (
+            abi.decode(_valuePast, (uint256)),
+            abi.decode(_valuePresent, (uint256))
+        );
+    }
 
     function getEthSpotPrice() public view returns (uint256) {
         bytes memory _queryData = abi.encode(
@@ -37,33 +63,19 @@ contract NFT is ERC721, UsingTellor {
         _safeMint(to, _tokenIds.current());
         ethPrice[_tokenIds.current()] = getEthSpotPrice();
         _tokenIds.increment();
-        emit BatchMetadataUpdate(0,type(uint256).max);
+        emit BatchMetadataUpdate(0, type(uint256).max);
     }
 
     function tokenURI(
         uint256 tokenId
     ) public view virtual override returns (string memory) {
         _requireMinted(tokenId);
-  
-       uint256 price = getEthSpotPrice();
-        if (ethPrice[tokenId] > price) {
-            return bearMetadata;
-        } else {
+
+        (uint256 yesterday, uint256 today) = getEthSpotPrices();
+        if (today >  yesterday) {
             return bullMetadata;
+        } else {
+            return bearMetadata;
         }
-    }
-    function turnBull(uint256 tokenId) external {
-        ethPrice[tokenId] = 0;
-          emit BatchMetadataUpdate(0,type(uint256).max);
-    }
-
-    function turnBear(uint256 tokenId) external {
-        ethPrice[tokenId] = type(uint256).max;
-          emit BatchMetadataUpdate(0,type(uint256).max);
-    }
-
-    function resetPreviwPrice(uint256 tokenId) external {
-        ethPrice[tokenId] = getEthSpotPrice();
-          emit BatchMetadataUpdate(0,type(uint256).max);
     }
 }
